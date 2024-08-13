1e7752c2e6e83bf94cf058a3679a421bcec3f09f
Khusnora <126550399+KhusnoraSobirova@users.noreply.github.com>
2024-08-13 18:46:34 +0900

    feat:prepare harid ejs page

--- server.js
+++ server.js
@@ -12,18 +12,22 @@ app.use(express.urlencoded({extended:true}));
 //2- Session code
 
 //3- View code
+
 app.set("views", "views");
 app.set("view engine", "ejs");
 
 //4 - Routing code
-app.get("/hello", function(req, res){
+
    // res.end(`<h1 style = "background: yellow ">HELLO WORLD by Nora</h1>`);
-   res.end(`<h1>HELLO WORLD</h1>`);
+   //res.render("harid");
+   
+  app.post("/create-item", (req,res) =>{
+    console.log(req.body);
+    res.json({ test:"success"});
+  });
+app.get("/", function(req,res) {
+    res.render("harid");
 });
-app.get("/gift", function(req, res){
-    // res.end(`<h1 style = "background: yellow ">HELLO WORLD by Nora</h1>`);
-    res.end(`<h1>You are in gift's page </h1>`);
- });
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function (){
